cfd63677da131c8d06700317473d439a2e81e2af
jbartolotti <jamesbartolotti@gmail.com>
2023-02-09 15:40:22 -0600

    first release
    xnatNotify scans the prearchive, compares to a contact list, and emails the recipients in the contact list the contents of the prearchive for their projects.

--- R/interface.R
+++ R/interface.R
@@ -3,18 +3,62 @@
 
 #' Create Template
 #'
-#' This function creates a template Contacts.csv file in the current directory.
-#' Contacts.csv contains the project IDs and contact names and emails that
-#' will be messaged by the scirpt
+#' This function creates a template Contacts.csv file in the selected directory
+#' (default = current directory).
+#' The file Contacts.csv contains the project IDs and contact names
+#' and emails that will be messaged by the script.
 #'
+#' @param string directory to save Contacts.csv. Default is current directory.
 #' @export
-function template(){
-
-
+template <- function(writedir = getwd()){
+  file.copy(
+    system.file('extdata','Contacts.csv', package = 'xnatNotify'),
+    file.path(writedir,'Contacts.csv'))
 }
 
 
-#Lo
-function loadContacts(){
+#' @export
+loadContacts <- function(file = NA){
+    myfile <- UTILS.getFile(file)
+    dat <- read.csv(myfile$path, stringsAsFactors = FALSE)
+    return(dat)
+}
 
+#' XNAT Notify
+#'
+#' This function takes a prearchive location and a contact list. It searches
+#' the prearchive for all projects found in the contact list. Then it scans
+#' each project folder. It compiles a list of all scans there and their
+#' file counts, and sends this information in email reports to all contacts.
+#' An optional master email address will receive a report on all projects.
+#'
+#' @param prearch_dir prearchive directory that contains project folders.
+#'     Default is current directory.
+#' @param contacts data.frame or string. Either a data frame containing columns
+#'     PROJECT, NAME, EMAIL, DO_FILECOUNT
+#'     or a path to a csv file containing that information. Default is to open
+#'     an interactive file selector to get the csv file.
+#' @param master Optional master email contact. If present, they will receive
+#'     an email containing all reports sent to contacts.
+#' @export
+xnatNotify <- function(prearch_dir = getwd(), contacts = NA, master = NA){
+  if(class(contacts) != 'data.frame' && (is.na(contacts) || class(contacts)=='character')){
+    contacts <- loadContacts(contacts)
   }
+
+  projects <- PROCESS.getProjects(prearch_dir)
+  if(!all(contacts$PROJECT %in% projects$studies)){
+      disp('WARNING: The following projects are present in the Contact data,\n
+               But are not located in the prearchive:\n',
+           paste(contacts$PROJECT[!contacts$PROJECT %in% projects$studies], collapse = ', '))
+  }
+
+  plist <- contacts$PROJECT[contacts$PROJECT %in% projects$studies]
+  if(length(plist) == 0){
+    warning('No projects in the Contact data are present in the prearchive. Quitting.')
+  } else {
+    reports <- PROCESS.generateReports(prearch_dir, plist, contacts)
+    PROCESS.mailReports(reports, contacts,master)
+    return(reports)
+  }
+}
---
+++ R/process.R
@@ -0,0 +1,56 @@
+
+
+basedir_prearch <- '/xnatdata/prearch'
+
+PROCESS.getProjects <- function(prearch_dir){
+  prearch <- list()
+  prearch$all <- dir(prearch_dir)
+  prearch$clinical <- prearch$all[unlist(lapply(prearch$all, function(x){length(grep('_',x))>0}))]
+  prearch$studies <- prearch$all[!(prearch$all %in% prearch$clinical)]
+  return(prearch)
+}
+
+PROCESS.generateReports <- function(prearch_dir, plist, contacts){
+  reports <- list()
+  for(pp in plist){
+    all_lines <- c(sprintf('Hello, %s',contacts$NAME[contacts$PROJECT == pp]),'',
+                   sprintf('PROJECT ID: %s',pp),
+                   sprintf('REPORT GENERATED ON: %s',Sys.time(),'')
+                   )
+    all_lines2 <- as.character()
+    scans <- dir(file.path(prearch_dir,pp))
+    delta_days <- unlist(lapply(scans,
+                                function(x)
+                                {
+                                  (as.numeric(Sys.time()) -
+                                     as.numeric(file.info(file.path(prearch_dir,pp,x))$ctime)
+                                  ) / 60 / 60 / 24
+                                }))
+    week_scans <- scans[delta_days <= 8]
+
+    if (length(week_scans) > 0)
+    {
+      all_lines = c(all_lines, '', sprintf('===ALL SCANS FOR PROJECT %s IN PREARCHIVE THAT WERE ADDED IN THE LAST WEEK===',pp))
+      all_lines = c(all_lines,paste('scan on',week_scans,'for',pp),'')
+    }
+    if (length(scans) > 0)
+    {
+      all_lines2 = c(all_lines2, '', sprintf('===ALL SCANS FOR PROJECT %s CURRENTLY IN PREARCHIVE===',pp))
+      all_lines2 = c(all_lines2,paste('scan on',scans,'for',pp),'')
+    }
+
+    reports[[as.character(pp)]] <- c(all_lines, all_lines2,'.')
+  }
+
+  return(reports)
+}
+PROCESS.mailReports <- function(reports, contacts, master){
+  for(pp in names(reports)){
+    try(sendmail('xnat_reporter@hbic-synapse2.kumc.edu',contacts$EMAIL[contacts$PROJECT == pp],'XNAT Report',c(reports[[pp]])),silent = TRUE)
+  }
+  if(!is.na(master)){
+    master_report <- do.call('c',reports)
+    try(sendmail('xnat_reporter@hbic-synapse2.kumc.edu',master,'XNAT Report',c(master_report)),silent = TRUE)
+
+    }
+}
